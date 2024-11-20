b7bb5586d15709a41bc623af4bdc9d45cd3efbd6
jesuscruz12 <134993609+jesuscruz12@users.noreply.github.com>
2024-11-20 00:21:22 -0600

    Update SocialLinksManager.js

--- src/components/SocialLinksManager.js
+++ src/components/SocialLinksManager.js
@@ -10,7 +10,7 @@ const SocialLinksManager = () => {
     useEffect(() => {
         const fetchLinks = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/api/social-links');
+                const response = await axios.get('https://backendgias.onrender.com/api/social-links');
                 setLinks(response.data);
             } catch (error) {
                 console.error('Error fetching social links:', error);
@@ -25,11 +25,11 @@ const SocialLinksManager = () => {
         try {
             if (editId) {
                 // Edit link
-                const response = await axios.put(`http://localhost:5000/api/social-links/${editId}`, newLink);
+                const response = await axios.put(`https://backendgias.onrender.com/api/social-links/${editId}`, newLink);
                 setLinks(links.map(link => (link._id === editId ? response.data : link)));
             } else {
                 // Add new link
-                const response = await axios.post('http://localhost:5000/api/social-links', newLink);
+                const response = await axios.post('https://backendgias.onrender.com/api/social-links', newLink);
                 setLinks([...links, response.data]);
             }
             resetForm();
@@ -44,7 +44,7 @@ const SocialLinksManager = () => {
     // Eliminar un enlace
     const deleteLink = async (id) => {
         try {
-            await axios.delete(`http://localhost:5000/api/social-links/${id}`);
+            await axios.delete(`https://backendgias.onrender.com/api/social-links/${id}`);
             setLinks(links.filter(link => link._id !== id));
         } catch (error) {
             console.error('Error deleting social link:', error);
