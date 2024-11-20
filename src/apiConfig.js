25055116e5ad7ad4a557a1ae7bb0254d2f1364ec
jesuscruz12 <134993609+jesuscruz12@users.noreply.github.com>
2024-11-20 01:54:15 -0600

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,3 @@
-const API_URL = 'https://backendgias.onrender.com'; // o la URL correcta del backend
+const API_URL = 'https://backendgias.onrender.com/api'; // o la URL correcta del backend
 
 export default API_URL;
